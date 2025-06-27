531a3fbe91b5c9baee70fc691a9a8bbfefc2f304
0d-fako <od.fakorede@gmail.com>
2025-06-27 08:04:11 +0100

    Blog API with relationship

--- server.js
+++ server.js
@@ -1,23 +1,39 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config(); 
 
 const authorRoutes = require('./routes/authorRoutes');
 const postRoutes = require('./routes/postRoutes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./api-docs/swagger.json');
 
 const app = express();
+
+
 app.use(cors());
 app.use(express.json());
 
+
 app.use('/authors', authorRoutes);
 app.use('/posts', postRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('🎉 Blog API is running. Visit <a href="/api-docs">/api-docs</a> for documentation.');
+});
+
+
+const PORT = process.env.PORT 
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(PORT, () =>
-      console.log(`Blog API running on http://localhost:${PORT}`)
+      console.log(`✅ Blog API running on http://localhost:${PORT}`)
     );
   })
-  .catch(err => console.error('DB connection error:', err.message));
+  .catch((err) => {
+    console.error('❌ DB connection error:', err.message);
+    process.exit(1);
+  });
